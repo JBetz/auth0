f66b3256547f366fdd69838f2f615ea80b4014a5
Joe Betz <joebetz91@gmail.com>
2018-07-24 16:07:16 +0200

    logging decode errors

--- src/Auth0/Request.hs
+++ src/Auth0/Request.hs
@@ -53,9 +53,9 @@ execRequest t (API m p) a b hs = do
     in case code of
       200 ->
         case eitherDecode body :: Either String c of
-          Left _ -> return resp
+          Left err -> putStrLn err >> return resp
           Right response -> return $ resp { resPayload = Just response }
       _   ->
         case eitherDecode body :: Either String Auth0Error of
-          Left _ -> return resp
+          Left err -> putStrLn err >> return resp
           Right err'     -> return $ resp { resError = Just err' }
